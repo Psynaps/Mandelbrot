b56eca67e5a18bab9c9a8d6f686e380bab3f9c23
Psynaps <michaelgunn7@gmail.com>
2016-08-26 22:41:15 -0700

    Add files via upload

--- HW1_Gunn.java
+++ HW1_Gunn.java
@@ -1,21 +1,51 @@
-/**
- * **@MichaelGunn *Data Structures and Algorithms
-**/
+/*
+ *Michael Gunn
+ *HW #1
+ *8/25/16
+ */
+
 class HW1_Gunn {
+
     public static void main(String[] args) {
         int columns = 150;
         int rows = 100;
-        if (args.length>0){
-            try{
-            columns = Integer.parseInt(args[0]);
-            rows = Integer.parseInt(args[1]);
+        double centerReal = 0.0;
+        double centerImaginary = 0.0;
+        if (args.length > 0) {
+            try {
+                for (int i = 0; i < args.length; i++) {
+                    String s = args[i];
+                    switch (s){
+			case "-re":
+                            centerReal = Double.parseDouble(args[i + 1]);
+                            break;
+                        case "-im":
+                            centerImaginary = Double.parseDouble(args[i + 1]);
+                            break;
+                        case "-width":
+                            //to write
+                            break;
+                        case "-size":
+                            //to write
+                            break;
+                        case "-iters":
+                            //to write
+                            break;
+                        case "-bail":
+                            //to write
+                            break;
+
+                    
+                    }
+                }
             }
             catch (Exception e){
                 System.out.println("Invalid Columns or Rows parameter");
             }
+                }
+//                Mandelbrot m = new Mandelbrot(rows, columns, 100);
+//                m.draw();
+        
+
+            }
         }
-        Mandelbrot m = new Mandelbrot (rows,columns,100);
-        m.draw();
-       
-    }
-}
--- Mandelbrot.java
+++ Mandelbrot.java
@@ -1,16 +1,17 @@
+
 public class Mandelbrot {
 
-    private int limit = 100;	// Maximum iterations for membership testing
-    private int rows = 100;	// Horizontal size (in pixels) of drawing
-    private int columns = 150;	// Vertical size (in pixels) of drawing
+    private static int limit = 255;	// Maximum iterations for membership testing
+    private static int rows = 100;	// Horizontal size (in pixels) of drawing
+    private static int columns = 150;	// Vertical size (in pixels) of drawing
+    private static int pixels = 500;
+    private static double size = 2.0;
 
-
-    public Mandelbrot(int rows, int columns, int iterations) {
+    public Mandelbrot(double centerReal, double centerImaginary, int rows, int columns, int iterations) {
         this.rows = rows;
         this.columns = columns;
         this.limit = iterations;
     }
-
 
     // Test to see if a point in the complex plane is a member of 
     // the Mandelbrot set.  A point (x) is in the set if the sequence
@@ -19,53 +20,49 @@ public class Mandelbrot {
     // modulus greater than 2.0, it will diverge.  Membership in the
     // set is approximated by checking that the sequence of the first
     // 100 elements of the series all have modulus <= 2.0
-
-    public boolean isMember(Complex x) {
-	Complex z = x;
+    public int isMember(Complex x) {
+        Complex z = x;
 
         for (int i = 0; i < limit; i++) {
-	    if (z.modulusSquared () > 4.0) return false;
-	    z = z.square().add(x);
-	}
+            if (z.modulusSquared() > 4.0) {
+                return i; //return i when it diverges
+            }
+            z = z.square().add(x);
+        }
 
-	return true;
+        return limit;//return the limit if it didn't diverge
     }
 
-
     public void draw() {
-	
- 	// Dimensions of the canvas in the complex plane.
 
+        // Dimensions of the canvas in the complex plane.
         double xMin = -2.0;
         double xMax = +1.0;
         double yMin = -1.0;
         double yMax = +1.0;
-	double xRange = xMax - xMin;
-	double yRange = yMax - yMin;
+        double xRange = xMax - xMin;
+        double yRange = yMax - yMin;
 
-	// Set the bounds of the canvas in the complex plane as
-	// well as the size of the canvas in pixels.
-
-	StdDraw.setCanvasSize(columns, rows);
+        // Set the bounds of the canvas in the complex plane as
+        // well as the size of the canvas in pixels.
+        StdDraw.setCanvasSize(size, size);
         StdDraw.setXscale(xMin, xMax);
-	StdDraw.setYscale(yMin, yMax);
-	StdDraw.setPenRadius(0); // One pixel wide.
+        StdDraw.setYscale(yMin, yMax);
+        StdDraw.setPenRadius(0); // One pixel wide.
 
-	// For each pixel in the image, determine its coordinates in
-	// the complex plane and check to see if it is in the Mandelbrot
+        // For each pixel in the image, determine its coordinates in
+        // the complex plane and check to see if it is in the Mandelbrot
         // set or not.  Paint the pixel black if it is in the set.
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                double re = xMin + xRange * (double) col / (double) columns;
+                double im = yMin + yRange * (double) row / (double) rows;
+                Complex z = new Complex(re, im);
 
-	for (int row = 0; row < rows; row++) {
-	    for (int col = 0; col < columns; col++) {
-		double re = xMin + xRange * (double) col / (double) columns;
-		double im = yMin + yRange * (double) row / (double) rows;
-		Complex z = new Complex (re, im);
-
-		if (isMember(z)) {
-		    StdDraw.point(re, im);
-		}
-	    }
+                if (isMember(z) == limit) {//if it didn't diverge
+                    StdDraw.point(re, im);
+                }
+            }
         }
     }
-
 }

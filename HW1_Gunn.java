d77e05d1af787d7d8999a1a270b8f6c467ddc6e0
Psynaps <michaelgunn7@gmail.com>
2016-08-28 00:11:43 -0700

    Add files via upload

--- HW1_Gunn.java
+++ HW1_Gunn.java
@@ -6,46 +6,93 @@
 
 class HW1_Gunn {
 
+    private static double centerReal = 0.0;
+    private static double centerImaginary = 0.0;
+    private static double width = 4.0;
+    private static int size = 150;
+    private static int iters = 100;
+    private static double bail = 1.0E15;
+    private static ColorPallete color = ColorPallete.BLACK;
+
+    public static enum ColorPallete {
+
+        BLACK,
+        GRAY,
+        CUSTOM
+    }
+
+    public static class BadIntegerException extends NumberFormatException {
+
+        public BadIntegerException(int value) {
+            super("Invalid value: " + value);
+        }
+    }
+
+    public static class BadDoubleException extends NumberFormatException {
+
+        public BadDoubleException(double value) {
+            super("Invalid value: " + value);
+        }
+    }
+
     public static void main(String[] args) {
-        int columns = 150;
-        int rows = 100;
-        double centerReal = 0.0;
-        double centerImaginary = 0.0;
         if (args.length > 0) {
-            try {
-                for (int i = 0; i < args.length; i++) {
-                    String s = args[i];
-                    switch (s){
-			case "-re":
-                            centerReal = Double.parseDouble(args[i + 1]);
-                            break;
+            for (int i = 0; i < args.length - 1; i++) {
+                String s = args[i];
+                try {
+                    switch (s) { //need to handle exceptions for numberFormat, BadInteger, BadDouble
+                        case "-re":
+                            setDouble(args[i], args[i + 1]);
+                            centerReal = Double.parseDouble(args[i + 1]); //all of these need to be done through trying a set method 
+                            break;                                        //that throws exceptions for bad args
                         case "-im":
+                            setDouble(args[i], args[i + 1]);
                             centerImaginary = Double.parseDouble(args[i + 1]);
                             break;
                         case "-width":
-                            //to write
+                            setInt(args[i], args[i + 1]);
+                            width = Double.parseDouble(args[i + 1]);
                             break;
                         case "-size":
-                            //to write
+                            setInt(args[i], args[i + 1]);
+                            size = Integer.parseInt(args[i + 1]);
                             break;
                         case "-iters":
-                            //to write
+                            setInt(args[i], args[i + 1]);
+                            iters = Integer.parseInt(args[i + 1]);
                             break;
                         case "-bail":
-                            //to write
+                            setInt(args[i], args[i + 1]);
+                            bail = Integer.parseInt(args[i + 1]);
                             break;
-
-                    
+                        case "-color":
+                            try {
+                                color = ColorPallete.valueOf(s);
+                            } catch (IllegalArgumentException e) {
+                                System.out.println(e);
+                            }
+                            break;
                     }
+                } catch (NumberFormatException e) {
+                    System.out.println(e);
                 }
-            }
-            catch (Exception e){
-                System.out.println("Invalid Columns or Rows parameter");
-            }
-                }
-//                Mandelbrot m = new Mandelbrot(rows, columns, 100);
-//                m.draw();
-        
-
             }
         }
+        Mandelbrot m = new Mandelbrot(0.0, 0.0, 1.0E20, 500, 500, "smooth", 4.0);
+        m.draw();
+    }
+
+    public static void setInt(String option, String arg) throws NumberFormatException {
+        int input = Integer.parseInt(arg);
+        if ((!option.equals("-re") || !option.equals("-im")) && input < 0) {
+            throw new BadIntegerException(input);
+        }
+    }
+
+    public static void setDouble(String option, String arg) throws NumberFormatException {
+        double input = Double.parseDouble(arg);
+        if ((!option.equals("-re") || !option.equals("-im")) && input < 0.0) {
+            throw new BadDoubleException(input);
+        }
+    }
+}
--- Mandelbrot.java
+++ Mandelbrot.java
@@ -1,16 +1,44 @@
 
+import java.awt.Color;
+
+
 public class Mandelbrot {
+    private int rows = 100;
+    private int columns = 150; //testing
+    
+    
+    private int limit = 255;	// Maximum iterations for membership testing
+    private int size = 150; //number of pixels
+    private double bailout = 1E20;
+    private double centerReal = 0.0;
+    private double centerImaginary = 0.0;
+    private boolean member = false;
+    private boolean escape = false; //can replace all 3 with an enum
+    private boolean smooth = false;
+    private double width = 4.0;
+    
 
-    private static int limit = 255;	// Maximum iterations for membership testing
-    private static int rows = 100;	// Horizontal size (in pixels) of drawing
-    private static int columns = 150;	// Vertical size (in pixels) of drawing
-    private static int pixels = 500;
-    private static double size = 2.0;
-
-    public Mandelbrot(double centerReal, double centerImaginary, int rows, int columns, int iterations) {
-        this.rows = rows;
-        this.columns = columns;
+    public Mandelbrot(double centerReal, double centerImaginary, double bailout, int iterations, int size, String member, double width) {
         this.limit = iterations;
+        this.bailout = bailout;
+        this.centerReal = centerReal;
+        this.centerImaginary = centerImaginary;
+        this.size = size;
+        
+        switch (member){
+            case "member":
+                this.member = true;
+                break;
+            case "escape":
+                this.escape = true;
+                break;
+            case "smooth":
+                this.smooth = true;
+                break;
+        }    
+        System.out.println("member: " + member);
+        System.out.println("escape: "+ escape);
+        System.out.println("smooth: " + smooth);
     }
 
     // Test to see if a point in the complex plane is a member of 
@@ -19,10 +47,20 @@ public class Mandelbrot {
     // It can be proved that once a value z[i] in this sequence has a
     // modulus greater than 2.0, it will diverge.  Membership in the
     // set is approximated by checking that the sequence of the first
-    // 100 elements of the series all have modulus <= 2.0
-    public int isMember(Complex x) {
+    // 100 elements of the series all have modulus <= 2.0\
+    public boolean isMember(Complex x){ //basic black and white escape calculator
         Complex z = x;
-
+        for (int i=0; i<limit; i++){
+            if (z.modulusSquared() > 4.0){
+                return false;
+            }
+            z = z.square().add(x);
+        }
+        return true;
+    }
+    
+    public int escapeTime(Complex x) {//basic escape time that returns i. Creates banding
+        Complex z = x;
         for (int i = 0; i < limit; i++) {
             if (z.modulusSquared() > 4.0) {
                 return i; //return i when it diverges
@@ -33,9 +71,24 @@ public class Mandelbrot {
         return limit;//return the limit if it didn't diverge
     }
 
+    public double smoothEscapeTime(Complex x) {//smooth escape time algorithem, no banding
+        Complex z = x;
+        for (int i=0; i<limit; i++){
+            if (z.modulusSquared()<bailout){
+                return z.modulusSquared();
+            }
+            z=z.square().add(x);
+        }
+        return z.modulusSquared();
+    }
     public void draw() {
+        System.out.println("drew1");
 
         // Dimensions of the canvas in the complex plane.
+//        double xMin = centerReal - width/2;
+//        double xMax = centerReal + width/2; //figure out what this is supposed to be
+//        double yMin = centerImaginary - width/2;
+//        double yMax = centerImaginary + width/2;
         double xMin = -2.0;
         double xMax = +1.0;
         double yMin = -1.0;
@@ -45,7 +98,7 @@ public class Mandelbrot {
 
         // Set the bounds of the canvas in the complex plane as
         // well as the size of the canvas in pixels.
-        StdDraw.setCanvasSize(size, size);
+        StdDraw.setCanvasSize(columns, rows);
         StdDraw.setXscale(xMin, xMax);
         StdDraw.setYscale(yMin, yMax);
         StdDraw.setPenRadius(0); // One pixel wide.
@@ -53,14 +106,33 @@ public class Mandelbrot {
         // For each pixel in the image, determine its coordinates in
         // the complex plane and check to see if it is in the Mandelbrot
         // set or not.  Paint the pixel black if it is in the set.
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < columns; col++) {
-                double re = xMin + xRange * (double) col / (double) columns;
-                double im = yMin + yRange * (double) row / (double) rows;
+        for (int row = 0; row < rows; row++) { //what should I replace rows/columns with?
+            for (int col = 0; col < columns ; col++) {
+                double re = xMin + xRange * (double) col / (double) columns; // to fix
+                double im = yMin + yRange * (double) row / (double) rows; //to fix
                 Complex z = new Complex(re, im);
 
-                if (isMember(z) == limit) {//if it didn't diverge
+                
+                if (escape){
+                    //colorize based on escapeTime returned value
+                }
+                else if (smooth){
+                    int n = escapeTime(z)%256;
+                    System.out.println("n: " + n);
+                    double d = n-Math.log10(Math.abs(z.modulusSquared()))/Math.log10(z.modulusSquared());
+                    d = (n*256)%256;
+//                    d/=n;
+                    System.out.println(d);
+                    Color color = new Color (n, n/2, n/4);
+                    StdDraw.setPenColor(color);
                     StdDraw.point(re, im);
+                    
+                    //colorize based on smoothEscapeTime returned value
+                }
+                else {
+                    if (isMember(z)) {//default case uses standard black and white colorization
+                        StdDraw.point(re, im);
+                    }
                 }
             }
         }
